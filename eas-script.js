df6bafca32f853531d44ec412961249fe53c1009
AdielTuson <adielt81@gmail.com>
2023-08-06 14:05:02 +0300

    Create a new grid from the size of the user's input
    I changed the setting of the grid item's size to be a function
    that will recieve an itemsAmount argument in order to create
    a greed based on the argument provided. I then wrote a new
    function resizeGrid that will recieve a variable from the user
    and pass it to setGrid().

--- eas-script.js
+++ eas-script.js
@@ -1,22 +1,34 @@
 
 const gridContainer = document.querySelector(".grid");
-    console.log(gridContainer);
-const numberOfDivs = 50*50; 
+console.log(gridContainer);
+let numberOfDivs = 16 * 16;
+console.log(numberOfDivs);
 const resizeGridBtn = document.querySelector("#resize-btn");
+
 //setting the grid item's height and width in percentage to be dynamic
-const gridItemWidth = 100/ Math.sqrt(numberOfDivs);
-const gridItemsHeight = gridItemWidth;
+function setGrid(itemsAmount) {
+    const gridItemWidth = 100 / Math.sqrt(itemsAmount);
+    const gridItemsHeight = gridItemWidth;
 
-for (let i = 0; i < numberOfDivs; i++) {
-    const gridDiv = document.createElement('div');
-    gridDiv.classList.add('grid-item');
-    gridDiv.style.width = `${gridItemWidth}%`;
-    gridDiv.style.paddingBottom = `${gridItemsHeight}%`;
-    gridContainer.appendChild(gridDiv);
+    for (let i = 0; i < itemsAmount; i++) {
+        const gridDiv = document.createElement('div');
+        gridDiv.classList.add('grid-item');
+        gridDiv.style.width = `${gridItemWidth}%`;
+        gridDiv.style.paddingBottom = `${gridItemsHeight}%`;
+        gridContainer.appendChild(gridDiv);
 
-    gridDiv.addEventListener('mouseover', () => gridDiv.classList.add('grid-item-hover'));
+        gridDiv.addEventListener('mouseover', () => gridDiv.classList.add('grid-item-hover'));
+    }
+
 }
+setGrid(numberOfDivs);
 
+resizeGridBtn.addEventListener('click', () => resizeGrid());
 
-    
-
+function resizeGrid() {
+    const userInput = prompt("Please select a number between 0 and 100 to determine the size of the grid:", 16)
+    numberOfDivs = (userInput ** 2);
+    console.log(numberOfDivs);
+    setGrid(numberOfDivs);
+    console.log(numberOfDivs);
+}
