a5729f9c85723e896a688d050b54fcc061d39951
AdielTuson <adielt81@gmail.com>
2023-08-06 13:12:59 +0300

    Create a dynamic square grid
    I removed the CSS grid item class, that way the grid item's
    size isn't fixed. I moved the size of the item to be calculated
    inside the JS file, based on the numberOfDivs variable. Now
    I can change the variable and the grid will change accordingly

--- eas-script.js
+++ eas-script.js
@@ -1,13 +1,22 @@
 
 const gridContainer = document.querySelector(".grid");
     console.log(gridContainer);
-const numberOfDivs = 16*16; 
+const numberOfDivs = 50*50; 
+const resizeGridBtn = document.querySelector("#resize-btn");
+//setting the grid item's height and width in percentage to be dynamic
+const gridItemWidth = 100/ Math.sqrt(numberOfDivs);
+const gridItemsHeight = gridItemWidth;
 
 for (let i = 0; i < numberOfDivs; i++) {
     const gridDiv = document.createElement('div');
-    gridDiv.classList.add('grid-item')
+    gridDiv.classList.add('grid-item');
+    gridDiv.style.width = `${gridItemWidth}%`;
+    gridDiv.style.paddingBottom = `${gridItemsHeight}%`;
     gridContainer.appendChild(gridDiv);
+
+    gridDiv.addEventListener('mouseover', () => gridDiv.classList.add('grid-item-hover'));
 }
+
 
     
 
